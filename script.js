0e0de2653dcb172dc5cfa9a7dea0edb86b58b3c7
Donna M. Ada <donnaada@icloud.com>
2023-02-24 09:20:23 +1000

    adding js for lab 07

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 // Gets myName through input
 function getName(){
-  let userName = "";
-  while (userName === "" || userName === null){
-  userName = prompt("Please Enter Your Name: ");
-  }
+  // let userName = "";
+  // while (userName === "" || userName === null){
+  // userName = prompt("Please Enter Your Name: ");
+  // }
+
+  let userName = prompt("Please Enter Your Name: ");
   return userName;
+
+  // while (userName === "" || userName === null){
+  // userName = prompt("Please Enter Your Name: ");
+  // }
+  // return userName;
 }
 
 //Ask user if they want a personalized experience
 function personalizedExp(){
   let choice = confirm("Do you want a personalized browsing experience?");
   let personalizedName;
-  
+
   console.log(choice);
 
+  //if user wants personalized experience (clicked okay)
   if (choice === true){
+    
     personalizedName = getName();
-    console.log('Your Name is ' + personalizedName);
+
+    //Check if personalizedName from getName() is blank
+    if (personalizedName === ""){
+      alert ("You didnt enter a name. Please try again");
+      getName();
+
+    //Check if personalizedName from getName() is null (cancel was clicked)
+    } else if (personalizedName === null){
+      alert ("Uh-oh! Looks like you changed your mind");
+
+      let tryAgain = confirm("Did you want to try again?");
+
+      if (tryAgain === true){
+        personalizedName = getName();
+
+        if (personalizedName === null || personalizedName === ""){
+          personalizedName = "Visitor"
+        }
+        
+      } else {
+        personalizedName = "Visitor";
+        // alert("3No problem, you'll be viewing as a " + personalizedName);
+      }
+
+    //Check if personalizedName from getName() had a value
+    } else {
+      console.log('Hi ' + personalizedName + " :)");
+    }
+
+   
+
+  //User clicked Cancel
   } else {
-    console.log('you selected no');
     personalizedName = "Visitor";
-    console.log('Your Name is ' + personalizedName);
+    alert('Not a problem!');
   }
+
+  alert("You are entering the site as " + personalizedName);
+  
 
   return personalizedName;
 }
